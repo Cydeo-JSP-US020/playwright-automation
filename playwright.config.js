945e190c737c29a6d14131b93feef2d4b4264a61
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2026-09-20 09:34:05 -0400

    Playwright Automation

--- playwright.config.js
+++ playwright.config.js
@@ -1,38 +1,30 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+   testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 1,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 2,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://spartan-app-new-nonsecure.onrender.com",
+    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-   // screenshot: "on-first-failure",
-   // video: {mode: "on-first-retry", size: {width: 1900, height: 1080}},
-
-    launchOptions: {
-      slowMo: 0, // slow down by given milliseconds
-    },
-
+    headless: false,
   },
 
   /* Configure projects for major browsers */
