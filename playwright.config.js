21e7fa81850b5d0b523b6c8b65cbefd5ae3bf4e3
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2026-09-20 11:29:56 -0400

    Playwright automation

--- playwright.config.js
+++ playwright.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: 1,
+  workers: 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -24,7 +24,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    headless: false,
+    //headless: false,
   },
 
   /* Configure projects for major browsers */
--- tests/day02/locators-intro.spec.js
+++ tests/day02/locators-intro.spec.js
@@ -1,12 +1,12 @@
 import {test} from "@playwright/test";
 
-test("Simple google test", async ({page}) => {
+test("Simple google test @failed", async ({page}) => {
     // test codes
     await page.goto("https://www.google.com");
 
     await page.waitForTimeout(3000);
 
-    let searchBox = page.locator("textarea[id='APjFqb']");
+    let searchBox = page.locator("//textarea[@class='gLFyf']");
 
     // await searchBox.type("CYDEO");
     await searchBox.fill("CYDEO");
@@ -20,12 +20,8 @@ test("Simple google test", async ({page}) => {
 });
 
 /*
-<textarea class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" title="Search" 
-value="" jsaction="paste:puy29d;" aria-label="Search" placeholder="" aria-autocomplete="both" 
-aria-expanded="false" aria-haspopup="false" autocapitalize="off" autocomplete="off" 
-autocorrect="off" id="APjFqb" maxlength="2048" name="q" role="combobox" rows="1" 
-spellcheck="false" data-ved="0ahUKEwizipyd9cOKAxWFEFkFHdRyI5MQ39UDCAc"></textarea>
+<textarea jsname="yZiJbe" class="gLFyf" aria-controls="Alh6id" aria-owns="Alh6id" autofocus="" aria-label="Search" placeholder="" aria-autocomplete="both" aria-expanded="false" aria-haspopup="false" autocapitalize="off" autocomplete="off" autocorrect="off" id="ti6dpd" maxlength="2048" name="q" role="combobox" rows="1" spellcheck="false" data-ved="0ahUKEwiw0aDNuP2WAxV8FFkFHR42Df8Q39UDCBI"></textarea>
 */
 
-//textarea[@class = 'gLFyf']
+
 
